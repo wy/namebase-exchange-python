3ed825fe934b8496e5c2e12a662f71b3769dee8a
xingyue <qixingyue@126.com>
2020-02-29 09:59:40 +0800

    添加安装依赖requests

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/wy/namebase-exchange-python",
     packages=setuptools.find_packages(),
+    install_requires = ['requests'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
